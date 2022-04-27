799789bc0fc0a9425166040f88671b3baf11b4d2
Rabia808 <myself.rabia808@gmail.com>
2022-04-27 23:46:52 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import "./App.css"
+import Slider from './Slider';
+import Slider2 from './Slider2';
+import Slider3 from './Slider3';
+import Slider4 from './Slider4';
+import Slider5 from './Slider5';
 
-import '../styles/App.css';
 
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
 
-  
   return (
     <div id="main">
-      
+      <Slider/>
+      <Slider2/>
+      <Slider3/>
+      <Slider4/>
+      <Slider5/>
     </div>
   )
 }
---
+++ src/components/Slider6.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import image1 from "./01.jpeg";
+import image2 from "./2.png";
+import image3 from "./3.png";
+import image4 from "./4.jpeg";
+import image5 from "./5.png";
+
+const Slider6 = () => {
+  return (
+    <div className='slider6'>
+        <div className='heading'>
+            <h1 className='black-line'>
+                Delighted Clients
+            </h1>
+            <div className='dash'></div>
+        </div>
+        <div className='image-slider'>
+            <img src='' alt=''/>
+            <img src='' alt=''/>
+            <img src='' alt=''/>
+            <img src='' alt=''/>
+            <img src='' alt=''/>
+
+        </div>
+       
+    </div>
+  )
+}
+
+export default Slider6
